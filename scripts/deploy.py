eed6fb30044ca87bbf34c469b43a4d247236179b
Julio Lima <lima.julio.cs@gmail.com>
2021-11-15 17:24:22 -0300

    Update brownie-config in json to frontend function

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,14 @@
 from scripts.helpful_scripts import get_account, get_contract
 from brownie import DappToken, TokenFarm, config, network
 from web3 import Web3
+import yaml
+import json
 
 
 KEPT_BALANCE = Web3.toWei(100, "ether")
 
 
-def deploy_token_farm_and_dapp_token():
+def deploy_token_farm_and_dapp_token(update_front_end=False):
     account = get_account()
     dapp_token = DappToken.deploy({"from": account})
     token_farm = TokenFarm.deploy(
@@ -33,6 +35,10 @@ def deploy_token_farm_and_dapp_token():
     }
 
     add_allowed_tokens(token_farm, dict_of_allowed_tokens, account)
+
+    if update_front_end:
+        update_front_end()
+
     return token_farm, dapp_token
 
 
@@ -49,5 +55,14 @@ def add_allowed_tokens(token_farm, dict_of_allowed_tokens, account):
     return token_farm
 
 
+def update_front_end():
+    # send the fron end our config in JSON format
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
+
+
 def main():
-    deploy_token_farm_and_dapp_token()
+    deploy_token_farm_and_dapp_token(update_front_end=True)
